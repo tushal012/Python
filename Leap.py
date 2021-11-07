54e59835a01b5214f2e2d1465da35f547e991fa1
tushal kumar <tushalkashyap11@gmail.com>
2021-11-07 12:48:38 +0530

    Added: Basic program of LeapYear

---
+++ Leap.py
@@ -0,0 +1,16 @@
+# to check leap year
+
+ 
+def checkYear(year):
+ 
+    # Return true if year is a multiple
+    # of 4 and not multiple of 100.
+    # OR year is multiple of 400.
+    return (((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0));
+ 
+
+year = 2020
+if(checkYear(year)):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
